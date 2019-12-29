fe87dbed443ea30810abaa9901a62e61619fb013
porridge34 <ivy.yan.ng@gmail.com>
2019-12-29 16:28:43 -0500

    clowned

--- js/app.js
+++ js/app.js
@@ -1,94 +1,52 @@
-window.onload = function() {
-    document.getElementById("add-item").onclick = addItem;
-    document.getElementById("remove-item").onclick = removeItem;
-}
-
-const printObject = function(objectHere) {
-  for (prop in objectHere){
-    console.log(objectHere[prop]);
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var text = document.createTextNode(inputValue);
+  li.appendChild(text);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("unorderList").appendChild(li);
   }
-};
+  document.getElementById("input").value = "";
+//removal button
+  var remove = document.createElement("BUTTON");
+  remove.innerHTML = "remove";
+  remove.className = "remove";
+  li.appendChild(remove);
+ //exclamation
+  var span = document.createElement("SPAN");
+  var exclamation = document.createTextNode("!");
+  span.className = "exclamation";
+  span.appendChild(exclamation);
+  li.appendChild(span);
 
-let theList = [];
-const addItem = function() {
-  let run = true;
-  let task = "";
-  while (run == true){
-    task = prompt("Enter a task: ");
-    if (task == null){
-      run = false;
-    }else {
-      let priority = "";
-      let runCheckPriority = false;
-      while (runCheckPriority == false){
-        priority = prompt("Is this high priority? Type yes or no.");
-        priority = priority.toLowerCase();
-        (priority == "yes" || priority == "no" || priority == null)? runCheckPriority = true : runCheckPriority = false;
-      }
-      if (priority != null){
-        let itemToAdd = Object.create({
-          "item": task,
-          "priority": priority,
-          "complete": false
-        });
-        (priority == "yes")? theList.unshift(itemToAdd) : theList.push(itemToAdd);
-        //adding the edit button
-        let editButton = document.createElement("BUTTON");
-        editButton.innerHTML = "click to edit";
-        function checkIndex(item){
-          item == itemToAdd;
-        }
-        editButton.id = theList.findIndex(checkIndex);
-        printObject(itemToAdd); //debug
-        //adding the text
-        let theTask = document.createElement("p");
-        theTask.id = theList.findIndex(checkIndex);
-        //checkbox
-        let checkbox = document.createElement("IMG");
-        checkbox.src = "images\\emptybox.jpg";
-        checkbox.id = theTask.id;
-        //container
-        let container = document.createElement("DIV");
-        container.id = theTask.id;
-        //adding the exclamation mark
-        if (priority == "yes"){
-          let exclamation = document.createElement("IMG");
-          exclamation.src = "images\\exclamation.jpg";
-          exclamation.id = theTask.id;
-          theTask.innerHTML = task;
-          container.innerHTML = theTask + checkbox + priority;
-          document.getElementById("items-in-list").prepend(container);
-        }else if (priority == "no"){
-          theTask.innerHTML = task;
-          container.innerHTML = theTask + checkbox + editButton;
-          document.getElementById("items-in-list").appendChild(container);
-        }
-        document.getElementById("items-in-list").style.pageBreakAfter = "always";
-      }
-      run = false;
+  var close = document.getElementsByClassName("remove");
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
   }
-};
-
-const removeItem = function() {
-  printObject(theList[0]);
-  let itemToRemove = "";
-  let run = true;
-  let storeSplice = [];
-  while (run == true){
-    itemToRemove = prompt("Enter the number of the item to be removed.");
-    if (itemToRemove == null){
-      run = false;
-    }else if (itemToRemove <= 0 || isNaN(itemToRemove) || itemToRemove > theList.length){
-      alert("Invalid response, try again."); //manual blinking and breathing
-    }else{
-      document.body.firstChild.remove();
-      storeSplice = theList.splice(itemToRemove-1, 1);
-      run = false;
+  // Add a "checked" symbol when clicking on a list item
+  var list = document.querySelector('ul');
+  list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
     }
-  }
-};
+  }, false);
 
-const markAsDone = function() {
-
+  var list = document.querySelector('ul');
+  list.addEventListener('click', function(ev) {
+    if (ev.target.className === 'exclamation') {
+      console.log("DDD");
+      ev.target.parentElement.classList.toggle('priority');
+      ev.target.classList.toggle('priority');
+      if (ev.target.class == 'priority exclamation' || ev.target.class == 'exclamation priority'){
+        document.getElementByType("ul").prepend(ev.target);
+        console.log("EEEE");
+      }
+    }
+  }, false);
 }
