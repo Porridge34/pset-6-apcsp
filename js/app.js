aa1e84d5e6c44c7e75126e9bae32b81798b85d36
porridge34 <ivy.yan.ng@gmail.com>
2020-01-01 12:39:43 -0500

    bug fix patchhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- js/app.js
+++ js/app.js
@@ -1,41 +1,3 @@
-var myNodelist = document.getElementsByTagName("LI");
-for (let i = 0; i < myNodelist.length; i++) {
-  var removeButton = document.createElement("BUTTON");
-  removeButton.className = "remove";
-  removeButton.innerHTML = "remove";
-  myNodelist.appendChild(removeButton);
-  var span2 = document.createElement("SPAN");
-  var exclamation2 = document.createTextNode("!");
-  span2.id = "exclamation";
-  span2.appendChild(exclamation2);
-  myNodelist.appendChild(span2);
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  console.log("this is running");
-  if (ev.target.tagName === 'LI'){
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.id == "exclamation") {
-    console.log(ev.target.id);
-    ev.target.classList.toggle('priority');
-    console.log(ev.target.className);
-    if (ev.target.className == 'priority'){
-      document.getElementById("unorderList").prepend(ev.target.parentElement);
-      console.log("EEEE");
-    }else{
-      document.getElementById("unorderList").append(ev.target.parentElement);
-      console.log("FFFFF");
-    }
-  }
-}, false);
-
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
@@ -67,5 +29,28 @@ function newElement() {
       div.remove();
     }
   }
+
+  li.addEventListener('click', function(ev) {
+    if (ev.target.id == "exclamation") {
+      console.log(ev.target.id);
+      ev.target.classList.toggle('priority');
+      console.log(ev.target.className);
+      if (ev.target.className == 'priority'){
+        document.getElementById("unorderList").prepend(ev.target.parentElement);
+        console.log("EEEE");
+      }else{
+        document.getElementById("unorderList").append(ev.target.parentElement);
+        console.log("FFFFF");
+      }
+    }
+  }, false);
+
+  // Add a "checked" symbol when clicking on a list item
+  li.addEventListener('click', function(ev) {
+    console.log("this is running");
+    if (ev.target.tagName === 'LI'){
+      ev.target.classList.toggle('checked');
+    }
+  }, false);
 
 }
