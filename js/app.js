cbb534406a79cecbef12a72480bbd2772d182ece
Ivy Ng <INg@ucvts.tec.nj.us>
2020-01-02 10:45:19 -0500

    added an array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 //adds element to list
+var array = [];
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("input").value;
+  array.push(inputValue);
   var text = document.createTextNode(inputValue);
   li.appendChild(text);
   if (inputValue === '') {
